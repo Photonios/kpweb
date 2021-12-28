74ab3a9cfcfb6ddde36c2756a6a45e1410a88961
Swen Kooij <swenkooij@gmail.com>
2021-12-28 23:30:22 +0200

    Quick and dirty UI for selecting an entry
    This needs to be split into small components and made to work. I
    jus sketched this out quickly to play with the UX.

--- client/src/entries/entryList.tsx
+++ client/src/entries/entryList.tsx
@@ -1,5 +1,20 @@
 import React from 'react';
-import { Table, Text } from 'evergreen-ui';
+import {
+  Pane,
+  Table,
+  Text,
+  SideSheet,
+  Paragraph,
+  Heading,
+  TextInput,
+  TextInputField,
+  FormField,
+  majorScale,
+  EyeOpenIcon,
+  ClipboardIcon,
+  IconButton,
+  TextareaField,
+} from 'evergreen-ui';
 
 import useMediaQuery from '../useMediaQuery';
 import { EntryDTO } from './types';
@@ -16,6 +31,7 @@ const EntryList = ({ entries }: Props) => {
 
   const [query, setQuery] = React.useState('');
   const [filteredEntries, setFilteredEntries] = React.useState(entries);
+  const [selectedEntry, setSelectedEntry] = React.useState(null);
 
   React.useEffect(() => {
     const normalizedQuery = normalize(query);
@@ -25,38 +41,107 @@ const EntryList = ({ entries }: Props) => {
   }, [query]);
 
   return (
-    <Table>
-      <Table.Head>
-        <Table.SearchHeaderCell value={query} onChange={setQuery} />
-        {isMediumWindow && (
-          <Table.TextHeaderCell>Username</Table.TextHeaderCell>
+    <>
+      <Table>
+        <Table.Head>
+          <Table.SearchHeaderCell value={query} onChange={setQuery} />
+          {isMediumWindow && (
+            <Table.TextHeaderCell>Username</Table.TextHeaderCell>
+          )}
+          {isLargeWindow && <Table.TextHeaderCell>URL</Table.TextHeaderCell>}
+        </Table.Head>
+        <Table.Body height="100%">
+          {filteredEntries.map((entry) => (
+            <Table.Row
+              key={entry.id}
+              isSelectable
+              onSelect={() => setSelectedEntry(entry)}
+            >
+              <Table.Cell>
+                <Text
+                  textOverflow="ellipsis"
+                  whiteSpace="nowrap"
+                  overflow="hidden"
+                  textAlign="left"
+                  style={{ direction: 'rtl' }}
+                >
+                  {[...entry.path, entry.name].join(' → ')}
+                </Text>
+              </Table.Cell>
+              {isMediumWindow && (
+                <Table.TextCell>{entry.username || '-'}</Table.TextCell>
+              )}
+              {isLargeWindow && (
+                <Table.TextCell>{entry.url || '-'}</Table.TextCell>
+              )}
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table>
+      <SideSheet
+        position={isMediumWindow ? 'right' : 'bottom'}
+        isShown={!!selectedEntry}
+        onBeforeClose={() => setSelectedEntry(null)}
+        preventBodyScrolling
+        shouldCloseOnEscapePress
+        shouldCloseOnOverlayClick
+        containerProps={{
+          display: 'flex',
+          flex: '1',
+          flexDirection: 'column',
+        }}
+      >
+        {selectedEntry && (
+          <>
+            <Pane
+              display="flex"
+              alignItems="center"
+              justifyContent="space-between"
+              zIndex={1}
+              flexShrink={0}
+              elevation={0}
+              backgroundColor="white"
+            >
+              <Pane padding={majorScale(2)} borderBottom="muted">
+                <Heading size={600}>{selectedEntry.name}</Heading>
+                <Paragraph size={400} color="muted">
+                  {selectedEntry.path.join(' → ')}
+                </Paragraph>
+              </Pane>
+            </Pane>
+            <Pane flex="1" overflowY="scroll" background="tint1" padding={16}>
+              <TextInputField
+                label="Username"
+                value={selectedEntry.username}
+                readOnly
+                width="100%"
+              />
+              <FormField label="Password" marginBottom={majorScale(3)}>
+                <Pane display="flex" alignItems="center">
+                  <TextInput
+                    type="password"
+                    value={'**********'}
+                    readOnly
+                    width="100%"
+                  />
+                  <IconButton icon={<EyeOpenIcon color="muted" size={20} />} />
+                  <IconButton
+                    icon={<ClipboardIcon color="muted" size={20} />}
+                  />
+                </Pane>
+              </FormField>
+              <TextInputField
+                label="URL"
+                value={selectedEntry.url}
+                readOnly
+                width="100%"
+              />
+              <TextareaField label="Notes" value="test" />
+            </Pane>
+          </>
         )}
-        {isLargeWindow && <Table.TextHeaderCell>URL</Table.TextHeaderCell>}
-      </Table.Head>
-      <Table.Body height="100%">
-        {filteredEntries.map((entry) => (
-          <Table.Row key={entry.id} isSelectable>
-            <Table.Cell>
-              <Text
-                textOverflow="ellipsis"
-                whiteSpace="nowrap"
-                overflow="hidden"
-                textAlign="left"
-                style={{ direction: 'rtl' }}
-              >
-                {[...entry.path, entry.name].join(' → ')}
-              </Text>
-            </Table.Cell>
-            {isMediumWindow && (
-              <Table.TextCell>{entry.username || '-'}</Table.TextCell>
-            )}
-            {isLargeWindow && (
-              <Table.TextCell>{entry.url || '-'}</Table.TextCell>
-            )}
-          </Table.Row>
-        ))}
-      </Table.Body>
-    </Table>
+      </SideSheet>
+    </>
   );
 };
 
