388ad2cbf716b3daa083935b1dfd72601bea6c7f
Swen Kooij <swenkooij@gmail.com>
2021-12-28 15:15:37 +0200

    Set hasSession state based on cookie
    TODO:
    
    * Do not hard-code the cookie name in the front-end.
    * Handle the session cookie being fucked.

--- state.ts
+++ state.ts
@@ -2,7 +2,10 @@ import { atom, selector } from "recoil";
 
 import { listEntries } from "./http";
 
-export const hasSessionState = atom({ key: "hasSession", default: false });
+export const hasSessionState = atom({
+  key: "hasSession",
+  default: document.cookie.includes("kpweb_session_active=1"),
+});
 
 export const entriesState = selector({
   key: "entries",
