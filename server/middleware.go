a2124e11cfb606b21a1a7be141dbfad2b7225813
Swen Kooij <swenkooij@gmail.com>
2021-12-30 11:48:01 +0200

    Set Strict-Transport-Security header

--- server/middleware.go
+++ server/middleware.go
@@ -45,6 +45,10 @@ func securityHeadersMiddleware(h http.Handler) http.Handler {
 		w.Header().Set("Permissions-Policy", "accelerometer=(), ambient-light-sensor=(), autoplay=(), battery=(), camera=(), cross-origin-isolated=(), display-capture=(), document-domain=(), encrypted-media=(), execution-while-not-rendered=(), execution-while-out-of-viewport=(), fullscreen=(), geolocation=(), gyroscope=(), keyboard-map=(), magnetometer=(), microphone=(), midi=(), navigation-override=(), payment=(), picture-in-picture=(), publickey-credentials-get=(), screen-wake-lock=(), sync-xhr=(), usb=(), web-share=(), xr-spatial-tracking=(), clipboard-read=(), clipboard-write=(), gamepad=(), speaker-selection=(), conversion-measurement=(), focus-without-user-activation=(), hid=(), idle-detection=(), interest-cohort=(), serial=(), sync-script=(), trust-token-redemption=(), window-placement=(), vertical-scroll=()")
 		w.Header().Set("Content-Security-Policy", "default-src 'self'; style-src 'self' 'unsafe-inline'")
 
+		if GetIsSecure() {
+			w.Header().Set("Strict-Transport-Security", "max-age=63072000; preload")
+		}
+
 		h.ServeHTTP(w, r)
 	})
 }
