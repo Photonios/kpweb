759acdbf9ab615c28eb4dac9cb75533ba84ffc74
Swen Kooij <swenkooij@gmail.com>
2021-12-30 21:58:51 +0200

    Improve search/filtering by tokenizing and debouncing
    Non-optimized version, but it'll do for now.

--- client/src/entries/useFilteredEntries.ts
+++ client/src/entries/useFilteredEntries.ts
@@ -1,17 +1,40 @@
 import React from 'react';
 
+import AwesomeDebouncePromise from 'awesome-debounce-promise';
+
 import { EntryDTO } from '../types';
 
-const normalize = (text: string): string => text.toLowerCase().replace(' ', '');
+const tokenize = (text: string): string[] =>
+  text
+    .trim()
+    .toLowerCase()
+    .replace(/[^0-9a-zA-Z_ ]/gi, '')
+    .split(' ');
+
+const filterEntries = AwesomeDebouncePromise(
+  (entries: EntryDTO[], query: string) => {
+    const queryTokens = tokenize(query);
+
+    return entries.filter(({ name, path }) => {
+      const entryTokens = [...path.map(tokenize), ...tokenize(name)];
+      return queryTokens.every((queryToken) =>
+        entryTokens.some((entryToken) => entryToken.includes(queryToken))
+      );
+    });
+  },
+  200
+);
 
 const useFilteredEntries = (entries: EntyDTO[], query: string): EntryDTO[] => {
-  const normalizedQuery = normalize(query);
   const [filteredEntries, setFilteredEntries] = React.useState(entries);
 
-  React.useEffect(() => {
-    setFilteredEntries(
-      entries.filter(({ name }) => normalize(name).includes(normalizedQuery))
-    );
+  React.useEffect(async () => {
+    if (!query) {
+      setFilteredEntries(entries);
+      return;
+    }
+
+    setFilteredEntries(await filterEntries(entries, query));
   }, [entries, query]);
 
   return filteredEntries;
