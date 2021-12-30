6fc2f452d8d4394e9f80b58a9dcd63fdcdc19e2e
Swen Kooij <swenkooij@gmail.com>
2021-12-30 12:30:45 +0200

    Add loading state for fetching password and prevent double fetching

--- client/src/controls/passwordInput.tsx
+++ client/src/controls/passwordInput.tsx
@@ -17,16 +17,27 @@ type Props = Omit<React.ComponentProps<typeof TextInput>, 'type' | 'value'> & {
 const PasswordInput = ({ onReveal, ...props }: Props) => {
   const [value, setValue] = React.useState(null);
   const [valueVisible, setValueVisible] = React.useState(false);
+  const [isLoading, setIsLoading] = React.useState(false);
+
+  const retrieve = async () => {
+    if (value) {
+      return value;
+    }
+
+    setIsLoading(true);
+    setValue(await onReveal());
+    setIsLoading(false);
+
+    return value;
+  };
 
   const reveal = async () => {
-    setValue(await onReveal());
+    await retrieve();
     setValueVisible(true);
   };
 
   const copy = async () => {
-    const retrievedValue = await onReveal();
-    setValue(retrievedValue);
-
+    const retrievedValue = await retrieve();
     navigator.clipboard.writeText(retrievedValue);
 
     sendToClipboard(
@@ -45,13 +56,16 @@ const PasswordInput = ({ onReveal, ...props }: Props) => {
         type={valueVisible ? 'text' : 'password'}
         value={value && valueVisible ? value : '***********'}
         readOnly
+        disabled={isLoading}
         {...props}
       />
       <IconButton
+        disabled={isLoading}
         icon={<ClipboardIcon color="muted" size={20} />}
         onClick={copy}
       />
       <IconButton
+        disabled={isLoading}
         icon={
           valueVisible ? (
             <EyeOnIcon color="muted" size={20} />
