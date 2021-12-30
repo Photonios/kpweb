18891fc89962ac84a38cb8b1137b8d4a0c1b86cd
Swen Kooij <swenkooij@gmail.com>
2021-12-30 11:37:47 +0200

    Set Feature-Policy header properly

--- server/middleware.go
+++ server/middleware.go
@@ -42,7 +42,7 @@ func securityHeadersMiddleware(h http.Handler) http.Handler {
 		w.Header().Set("X-Content-Type-Options", "nosniff")
 		w.Header().Set("Referrer-Policy", "no-referrer")
 		w.Header().Set("Clear-Site-Data", "\"*\"")
-		w.Header().Set("Feature-Policy", "none")
+		w.Header().Set("Feature-Policy", "accelerometer 'none'; ambient-light-sensor 'none'; autoplay 'none'; battery 'none'; camera 'none'; display-capture 'none'; document-domain 'none'; encrypted-media 'none'; fullscreen 'none'; gamepad 'none'; geolocation 'none'; gyroscope 'none'; layout-animations 'none'; legacy-image-formats 'none'; magnetometer 'none'; microphone 'none'; midi 'none'; oversized-images 'none'; payment 'none'; picture-in-picture 'none'; publickey-credentials-get 'none'; speaker-selection 'none'; sync-xhr 'none'; unoptimized-images 'none'; unsized-media  'none'; usb 'none'; screen-wake-lock 'none'; web-share 'none'; xr-spatial-tracking 'none'")
 
 		h.ServeHTTP(w, r)
 	})
