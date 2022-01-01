ce28174f48be6101aa96ffe188b87b9230ec6e54
Swen Kooij <swenkooij@gmail.com>
2022-01-01 22:40:24 +0200

    Automatically log out when cookie gets deleted/expires

--- client/src/app/state.ts
+++ client/src/app/state.ts
@@ -5,6 +5,19 @@ export const hasSessionState = atom({
   key: 'hasSession',
   // TODO: do not hard-code cookie name
   default: document.cookie.includes('kpweb_session_active=1'),
+  effects_UNSTABLE: [
+    ({ setSelf }) => {
+      const interval = setInterval(() => {
+        if (document.cookie.includes('kpweb_session_active=1')) {
+          setSelf(true);
+        } else {
+          setSelf(false);
+        }
+      }, 500);
+
+      return () => clearInterval(interval);
+    },
+  ],
 });
 
 export const entriesState = selector({
