c32edddf9da821266527b553f2860e9e1184558e
Swen Kooij <swenkooij@gmail.com>
2022-02-20 16:02:06 +0200

    Fix tag colors being random
    'automatic' means evergreen-ui will pick a random color. This is
    annoying me.

--- client/src/ui/entry/entryTags.tsx
+++ client/src/ui/entry/entryTags.tsx
@@ -1,18 +1,40 @@
 import React from 'react';
 import { Badge, majorScale } from 'evergreen-ui';
+import { Theme, useTheme } from 'evergreen-ui';
 
 interface Props {
   tags: string[];
 }
 
-const EntryTags = ({ tags }: Props) => (
-  <>
-    {tags.map((tag) => (
-      <Badge key={tag} color="automatic" marginRight={majorScale(1)}>
-        {tag}
-      </Badge>
-    ))}
-  </>
-);
+const createHashCode = (value: string): number => {
+  let hashCode = 0;
+
+  for (let i = 0; i < value.length; i++) {
+    hashCode = value.charCodeAt(i) + ((hashCode << 5) - hashCode);
+  }
+
+  return hashCode;
+};
+
+const pickColor = (theme: Theme, value: string): string => {
+  const hashCode = createHashCode(value);
+  const availableColors = Object.keys(theme.fills);
+
+  return availableColors[hashCode % availableColors.length];
+};
+
+const EntryTags = ({ tags }: Props) => {
+  const theme = useTheme();
+
+  return (
+    <>
+      {tags.map((tag) => (
+        <Badge key={tag} color={pickColor(theme, tag)} marginRight={majorScale(1)}>
+          {tag}
+        </Badge>
+      ))}
+    </>
+  );
+};
 
 export default EntryTags;
